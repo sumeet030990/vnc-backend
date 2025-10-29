fe27b79c2ab4dcea2a0358f5dfe0c849c01bc572
Sumeet Jadhav <Sumeet.Jadhav@accionlabs.com>
2025-10-30 00:42:06 +0530

    Update .gitignore to include .venv; remove database configuration file and load DATABASE_URL from environment variables

--- database/config.py
+++
@@ -1,4 +0,0 @@
-# Placeholder for configuration settings
-# Example MySQL connection string:
-# mysql+pymysql://username:password@localhost:3306/dbname
-DATABASE_URL = "mysql+pymysql://root:password@localhost:3306/vnc"
--- database/database.py
+++ database/database.py
@@ -1,6 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from database.config import DATABASE_URL
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
